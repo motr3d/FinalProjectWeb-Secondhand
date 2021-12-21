3cb82cee944b53bdce4b8ee3f6605a22c35a624f
Gsaka <gatotsasangkataka@gmail.com>
2021-12-21 11:01:33 +0700

    Reverse That in your mind

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@ include "topheader.php";
       <div class="content">
         <div class="container-fluid">
          <div class="panel-body">
-		<a>
+		      <a>
             <?php  //success message
             if(isset($_POST['success'])) {
             $success = $_POST["success"];
@@ -70,40 +70,6 @@ include "topheader.php";
                             $count=$row["count_items"];
                             $i++;
                         echo "<tr><td>$cat_id</td><td>$cat_title</td><td>$count</td>
-
-                        </tr>";
-                        }
-                        ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title">Brands List</h4>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table table-hover tablesorter " id="">
-                    <thead class=" text-primary">
-                        <tr><th>ID</th><th>Merek</th><th>Count</th>
-                    </tr></thead>
-                    <tbody>
-                      <?php 
-                        $result=mysqli_query($con,"select * from merek")or die ("query 1 incorrect.....");
-                        $i=1;
-                        while(list($brand_id,$nama_brand)=mysqli_fetch_array($result))
-                        {	
-                            
-                            $sql = "SELECT COUNT(*) AS count_items FROM barang WHERE merek_barang=$i";
-                            $query = mysqli_query($con,$sql);
-                            $row = mysqli_fetch_array($query);
-                            $count=$row["count_items"];
-                            $i++;
-                        echo "<tr><td>$brand_id</td><td>$nama_brand</td><td>$count</td>
 
                         </tr>";
                         }
