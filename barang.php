f2f8056882eead2c7065634875e13954b77d41ea
motr3d <95088353+motr3d@users.noreply.github.com>
2021-12-26 23:47:22 +0700

    Update barang.php
    perbaikan harga dan beberapa detail kecil

--- barang.php
+++ barang.php
@@ -80,15 +80,15 @@ include "header.php";
                                     </div>
 
                                     <div class="product-preview">
-                                        <img src="product_images/'.$row['gambar_barang'].'" alt="">
+                                        <img src="product_images/'.$row['gambar_barang2'].'" alt="">
                                     </div>
 
                                     <div class="product-preview">
-                                        <img src="product_images/'.$row['gambar_barang'].'" alt="">
+                                        <img src="product_images/'.$row['gambar_barang3'].'" alt="">
                                     </div>
 
                                     <div class="product-preview">
-                                        <img src="product_images/'.$row['gambar_barang'].'" alt="">
+                                        <img src="product_images/'.$row['gambar_barang4'].'" alt="">
                                     </div>
                                 </div>
                             </div>
@@ -100,15 +100,15 @@ include "header.php";
                                     </div>
 
                                     <div class="product-preview">
-                                        <img src="product_images/'.$row['gambar_barang'].'" alt="">
+                                        <img src="product_images/'.$row['gambar_barang2'].'" alt="">
                                     </div>
 
                                     <div class="product-preview">
-                                        <img src="product_images/'.$row['gambar_barang'].'g" alt="">
+                                        <img src="product_images/'.$row['gambar_barang3'].'g" alt="">
                                     </div>
 
                                     <div class="product-preview">
-                                        <img src="product_images/'.$row['gambar_barang'].'" alt="">
+                                        <img src="product_images/'.$row['gambar_barang4'].'" alt="">
                                     </div>
                                 </div>
                             </div>
@@ -138,7 +138,7 @@ include "header.php";
 								<a class="review-link" href="#review-form">10 Review(s) | Add your review</a>
 							</div>
 							<div>
-								<h3 class="product-price">$'.$row['harga_barang'].'<del class="product-old-price">$990.00</del></h3>
+								<h3 class="product-price">Rp '.$row['harga_barang' ].'</h3>
 								<span class="product-available">In Stock</span>
 							</div>
 							<p>'.$row['deskripsi_barang'].'</p>
@@ -474,14 +474,14 @@ include "header.php";
 									<div class='product-img'>
 										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
 										<div class='product-label'>
-											<span class='sale'>-30%</span>
-											<span class='new'>NEW</span>
+
 										</div>
 									</div></a>
 									<div class='product-body'>
 										<p class='product-category'>$cat_name</p>
 										<h3 class='product-name header-cart-item-name'><a href='barang.php?p=$pro_id'>$pro_title</a></h3>
-										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>$990.00</del></h4>
+										
+										<h4 class='product-price header-cart-item-info'>Rp $pro_price</h4>
 										<div class='product-rating'>
 											<i class='fa fa-star'></i>
 											<i class='fa fa-star'></i>
@@ -489,11 +489,7 @@ include "header.php";
 											<i class='fa fa-star'></i>
 											<i class='fa fa-star'></i>
 										</div>
-										<div class='product-btns'>
-											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>add to wishlist</span></button>
-											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>add to compare</span></button>
-											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
-										</div>
+										
 									</div>
 									<div class='add-to-cart'>
 										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
